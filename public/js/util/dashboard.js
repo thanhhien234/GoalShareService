22f65a39687295c1cce72ff97863568046c233ba
thanhhien234 <doanthanhhien01@gmail.com>
2024-03-06 23:58:13 +0900

    fix: change img to div

--- public/js/util/dashboard.js
+++ public/js/util/dashboard.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     function moveGroupSearchContainer() {
-        if ($(window).width() < 900) {
+        if ($(window).width() < 768) {
             var groupSearchContainer = $('.group-search-container').detach();
             $('.my-week-routine').after(groupSearchContainer);
         } else {
