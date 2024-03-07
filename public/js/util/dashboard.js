d205fa4f244826e0dd5ff95459f430326e917976
thanhhien234 <doanthanhhien01@gmail.com>
2024-03-07 15:59:26 +0900

    fix: modify dashboard.js

--- public/js/util/dashboard.js
+++ public/js/util/dashboard.js
@@ -1,10 +1,13 @@
-// 더보기 버튼
 const detailBtn = document.querySelector('.detail-btn-container');
 const mobileDetailBtnContainer = document.querySelector('.mobile-detail-btn-container');
 const routineContainer = document.querySelector('.today-routine-container');
 const calendarContainer = document.querySelector('.calendar-routine');
+const groupSearchContainer = document.querySelector('.group-search-container');
+const myWeekRoutine = document.querySelector('.my-week-routine');
+const mainContent = document.querySelector('.main-content');
 
 
+// 더보기 버튼
 detailBtn.addEventListener('click', function() {
     calendarContainer.classList.add('show-details');
     routineContainer.classList.add('show-details');
@@ -14,10 +17,9 @@ mobileDetailBtnContainer.addEventListener('click', function() {
 });
 
 
+
+//mobile group-search-container
 function moveGroupSearchContainer() {
-    const groupSearchContainer = document.querySelector('.group-search-container');
-    const myWeekRoutine = document.querySelector('.my-week-routine');
-    const mainContent = document.querySelector('.main-content');
     if (window.innerWidth < 768) {
         myWeekRoutine.after(groupSearchContainer);
         calendarContainer.classList.add('show-details');
@@ -25,9 +27,7 @@ function moveGroupSearchContainer() {
         mainContent.before(groupSearchContainer);
     }
 }
-
 moveGroupSearchContainer();
-
 window.addEventListener('resize', function() {
     moveGroupSearchContainer();
 });
