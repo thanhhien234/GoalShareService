55b6c6cdb8695c1b611c785ddb7ffda64691dba2
thanhhien234 <doanthanhhien01@gmail.com>
2024-03-07 15:09:10 +0900

    fix: modify dashboard.js

--- public/js/util/dashboard.js
+++ public/js/util/dashboard.js
@@ -1,23 +1,3 @@
-$(document).ready(function() {
-    function moveGroupSearchContainer() {
-        if ($(window).width() < 768) {
-            var groupSearchContainer = $('.group-search-container').detach();
-            $('.my-week-routine').after(groupSearchContainer);
-            calendarContainer.classList.add('show-details');
-        } else {
-            var groupSearchContainer = $('.group-search-container').detach();
-            $('.main-content').before(groupSearchContainer);
-        }
-    }
-
-    moveGroupSearchContainer();
-    
-    $(window).resize(function() {
-        moveGroupSearchContainer();
-    });
-});
-
-
 // 더보기 버튼
 const detailBtn = document.querySelector('.detail-btn-container');
 const mobileDetailBtnContainer = document.querySelector('.mobile-detail-btn-container');
@@ -31,6 +11,25 @@ detailBtn.addEventListener('click', function() {
 });
 mobileDetailBtnContainer.addEventListener('click', function() {
     routineContainer.classList.add('show-details');
+});
+
+
+function moveGroupSearchContainer() {
+    const groupSearchContainer = document.querySelector('.group-search-container');
+    const myWeekRoutine = document.querySelector('.my-week-routine');
+    const mainContent = document.querySelector('.main-content');
+    if (window.innerWidth < 768) {
+        myWeekRoutine.after(groupSearchContainer);
+        calendarContainer.classList.add('show-details');
+    } else {
+        mainContent.before(groupSearchContainer);
+    }
+}
+
+moveGroupSearchContainer();
+
+window.addEventListener('resize', function() {
+    moveGroupSearchContainer();
 });
 
 
