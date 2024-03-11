90fe5cef35323b604a8c6f153f80af593453a108
thanhhien234 <doanthanhhien01@gmail.com>
2024-03-12 02:04:23 +0900

    feat: routineRank observer

---
+++ public/js/components/RoutineRank.js
@@ -0,0 +1,51 @@
+class RoutineRank extends Observer {
+    constructor() {
+      super();
+      this.popularRoutine = [];
+      this.colors = ['#C77DFF','#D9ED92','#C8B6FF','#5A189A','#DDE7C7','#7B2CBF', '#9747FF','#B5E48C','#E0AAFF','#5A189A']
+    }
+    
+    getPopularRoutine() {
+        return this.popularRoutine;
+    }
+    
+    setPopularRoutine(popularRoutine) {
+        this.popularRoutine = popularRoutine;
+    }
+    render(){
+        const popularRoutineContainer = document.querySelector(".routine-ranking-container");
+        popularRoutineContainer.innerHTML = "";
+        
+        let maxCount = this.popularRoutine.length > 0 ? this.popularRoutine[0].memberCount : 0;
+
+        this.popularRoutine.forEach((routine, index) => {
+            const routineElement = document.createElement("div");
+            routineElement.classList.add("routine-ranking-wrapper");
+
+            
+            const progressBarWidth = (routine.memberCount / maxCount) * 100;
+
+            routineElement.innerHTML = `
+                <div class="ranking-title">
+                    <span class="ranking">${index + 1}</span>
+                    <p class="routine-title">${routine.title}</p>
+                    <button class="detail-routine-btn"></button>
+                </div>
+                <div class="routine-progress-bar">
+                <div class="progress-bar-inner" style="background-color: ${this.colors[index]}; width: ${progressBarWidth}%;"></div>
+                    <span class="data">${routine.memberCount}</span>
+                </div>
+                <div class="routine-participants">
+                    <img src="/public/assets/image/face-icon.png">
+                    <span class="participants-num" style="color: ${this.colors[index]};">${routine.memberCount}명</span><span class="participants-text">이 참여 중이에요!</span>
+                </div>
+            `;
+    
+            popularRoutineContainer.appendChild(routineElement);
+    });
+  }
+  notify(){
+    this.setPopularRoutine(routine.popularRoutine);
+    this.render();
+  }
+}
